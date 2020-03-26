e267d250a80488e4b9224552de2382e6ef154734
Nimish <nimishgupta18396@gmail.com>
2020-03-26 16:53:16 +0530

    fix: removed export as pdf

--- src/components/activitybar/index.js
+++ src/components/activitybar/index.js
@@ -53,14 +53,6 @@ const ActivityBar = (props) => {
 					target="_blank">
 					<Github />
 				</Link>
-				<Link
-					title="Export as PDF"
-					variants={Item}
-					whileHover={{ scale: 1.2 }}
-					href="/download"
-					target="_blank">
-					<Pdf className="pdf" />
-				</Link>
 			</motion.ul>
 
 			<Mode whileHover={{ scale: 1.2 }} title="Change Theme">
